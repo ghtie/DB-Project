ddf4ef7acbbe80a2b0c81595a2ac082dec05485d
Madison Lucas <madisonlucas905@Madisons-MacBook-Pro.local>
2019-04-13 13:10:23 -0400

    update

--- run_app.py
+++ run_app.py
@@ -4,10 +4,35 @@ import configparser
 from flask import Flask, render_template, request
 import mysql.connector
 
-config = configparser.ConfigParse()
+# read configuration from file
+config = configparser.ConfigParser()
 config.read('config.ini')
 
-app = Flask(__name_)
+# set up application server
+app = Flask(__name__)
+
+# create a function for fetching data from database
+def sql_query(sql):
+    db = mysql.connector.connect(**config['mysql.connector'])
+    cursor = db.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return result
+
+def sql_execute(sql):
+    db = mysql.connector.connect(**config['mysql.connector'])
+    cursor = db.cursor()
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
+
+# not sure if this is the best app.route function to use yet
+@app.route('/')
+def basic_response():
+    return "Eyyyy, the web app runs!"
 
 if __name__ == '__main__':
     app.run()
