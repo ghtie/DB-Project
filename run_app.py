7a141283e27ff88eac64fe47a1b1ae61824ac582
Madison Lucas <madisonlucas905@Madisons-MacBook-Pro.local>
2019-04-22 22:30:06 -0400

    displays meal swipe info

--- run_app.py
+++ run_app.py
@@ -1,57 +1,69 @@
+# application to run program from terminal
+from __future__ import print_function
+from mysql.connector import errorcode
+
 import configparser
-from flask import Flask, render_template, request
+from flask import Flask, render_template, Response, request, redirect, url_for
 import mysql.connector
 
-# Read configuration from file.
+
+# read configuration from file
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# Set up application server.
+# set up application server
 app = Flask(__name__)
 
-# Create a function for fetching data from the database.
-def sql_query(sql):
-    db = mysql.connector.connect(**config['mysql.connector'])
-    cursor = db.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    cursor.close()
-    db.close()
-    return result
+#Define function to query database, returns result of query
+def db_query(sql):
+	cnx = mysql.connector.connect(user = 'team_17', password = 
+ 	'2004d2a4', port = '3306', host = 'eecslab-9.case.edu', database = 
+	'team_17')
+	cursor = cnx.cursor()
+	
+	cursor.execute(sql) #sql is the command, as a string literal
+	result = cursor.fetchall() #returns all results of the query
+
+	cursor.close()
+	cnx.close()
+	return result
+
+#Define function to write to database
+def db_write(sql):
+	cnx = mysql.connector.connect(user = 'team_17', password = 
+ 	'2004d2a4', port = '3306', host = 'eecslab-9.case.edu', database = 
+	'team_17')
+	cursor = cnx.cursor()
+	cursor.execute(sql)
+	cursor.commit()
+	cursor.close()
+	cnx.close()
+
+@app.route('/')
+def home():
+	return render_template('home.html')
 
 
-def sql_execute(sql):
-    db = mysql.connector.connect(**config['mysql.connector'])
-    cursor = db.cursor()
-    cursor.execute(sql)
-    db.commit()
-    cursor.close()
-    db.close()
+@app.route('/goToBuyPage', methods=['GET', 'POST'])
+def go_to_buy_page():
+	print(request.form)
+	if "seeBuyerView" in request.form:
+		template_data = {}
+		sql = "select user_id, price from swipes2 order by price"
+		swipes = db_query(sql)
+		template_data['swipes'] = swipes
+		print(swipes)
+		return render_template('buyPage.html', template_data=template_data)
+		#return ''
 
-# For this example you can select a handler function by
-# uncommenting one of the @app.route decorators.
-
-#@app.route('/')
-def basic_response():
-    return "It works!" #example
-
-#@app.route('/')
-def template_response():
-    return render_template('home.html')
-
-@app.route('/', methods=['GET', 'POST'])
-def template_response_with_data():
-    print(request.form)
-    if "buy-book" in request.form:
-        [book]_id = int(request.form["buy-book"])
-        sql = "delete from book where id={book_id}".format(book_id=book_id)
-        sql_execute(sql)
-    template_data = {}
-    sql = "select id, title from book order by title"
-    books = sql_query(sql)
-    template_data['books'] = books
-    return render_template('home-w-data.html', template_data=template_data)
+@app.route('/goToSellPage', methods=['GET', 'POST'])
+def go_to_sell_page():
+	print(request.form)
+	if "seeSellerView" in request.form:
+		return render_template('sellPage.html'), 400
+	else:
+		return ''
 
 if __name__ == '__main__':
     app.run()
-    #app.run(**config['app'])
+    #app.run(**config['app']) will probably need this one rather than app.run()
