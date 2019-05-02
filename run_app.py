4fdf512ac87c770305abc0d99744d466c84cecc4
bbaierl <bgb31@case.edu>
2019-05-02 10:15:34 -0400

    added buyer login, updated buypage hmtl as well as home html and edited run_app

--- run_app.py
+++ run_app.py
@@ -22,6 +22,8 @@ app = Flask(__name__)
 # landing page (keeps track of guest_user_id's).
 guest_id_counter = -1
 
+guest_case_id = ""
+
 # This acts as a boolean global variable that is 1 (inital entry into buy page) or 0 (buyer
 # is making more than one purchase so guest_id_counter should not be incremented).
 x = 1
@@ -76,8 +78,17 @@ def go_to_buy_page():
 	guest_id_counter = guest_id_counter + 1
 	print(guest_id_counter)
 	if "seeBuyerView" in request.form:
-		swipe_view_data = {}
-		return display_swipes_for_buyer(swipe_view_data, guest_id_counter=guest_id_counter)
+		id = str(request.form["caseID"])
+		#Error checking
+		if (id == ""):
+			nameErr = "Please fill out all fields"
+			return render_template('buyerLogin.html'), 400
+		else:
+			global guest_case_id
+			guest_case_id = id
+			print(guest_case_id)
+			swipe_view_data = {}
+			return display_swipes_for_buyer(swipe_view_data, guest_id_counter=guest_id_counter, guest_case_id = guest_case_id)
 
 # This is the route to redisplay the buyer view when a swipe is purchased.
 @app.route('/viewSwipes', methods=['GET', 'POST'])
@@ -90,16 +101,16 @@ def buy_swipe():
 			db_write(sql)
 			global x
 			if x == 1:
-				sql2 = "insert into guest_user (id, name) values (%d, 'Bob')" % guest_id_counter
+				sql2 = "insert into guest_user (id, name) values (%d, '%s')" % (guest_id_counter, guest_case_id)
 				db_write(sql2)
 				x = 0
 			sql3 = "update swipes set guest_user_id=%d where id=%s" % (guest_id_counter, swipe_id)
 			db_write(sql3)
 			buy_view_data = {}
-			return display_swipes_for_buyer(buy_view_data, guest_id_counter=guest_id_counter)
+			return display_swipes_for_buyer(buy_view_data, guest_id_counter=guest_id_counter, guest_case_id=guest_case_id)
 
 # This is a helper method to display the list of swipes on the buyer view page.
-def display_swipes_for_buyer(dictionary, guest_id_counter):
+def display_swipes_for_buyer(dictionary, guest_id_counter, guest_case_id):
 	dictionary = {}
 	sql = "select id, quantity, price from swipes where status=0 order by price"
 	swipes = db_query(sql)
@@ -108,7 +119,7 @@ def display_swipes_for_buyer(dictionary, guest_id_counter):
 	dataAvg = data[0][0]
 	dataSum = data[0][1]
 	dictionary['swipes'] = swipes
-	return render_template('buyPage.html', template_data=dictionary, dataAvg=dataAvg, dataSum=dataSum, guest_id_counter=guest_id_counter)
+	return render_template('buyPage.html', template_data=dictionary, dataAvg=dataAvg, dataSum=dataSum, guest_id_counter=guest_id_counter, guest_case_id = guest_case_id)
 
 # This is a helper method to display the list of swipes on the seller view page.
 def display_swipes_for_seller(dictionary, user_id):
@@ -124,6 +135,13 @@ def go_to_sell_page():
 	print(request.form)
 	if "seeSellerView" in request.form:
 		return render_template('sellerLogin.html'), 400
+		
+# This is the route to display the buyer view page
+@app.route('/goToBuyerLogin', methods=['GET', 'POST'])
+def go_to_buy_login():
+	print(request.form)
+	if "seeBuyerView" in request.form:
+		return render_template('buyerLogin.html'), 400
 
 # This is the route for the seller to identify themselves.
 @app.route('/createListing', methods=['GET', 'POST'])
@@ -142,6 +160,7 @@ def add_seller_info():
 				sql2 = "insert into user (name, ID) values ('%s', '%s')" %(name, id)
 				db_write(sql2)
 			return render_template('sellerList.html', name=name, id=id), 400
+			
 
 # This is the route to post the swipe a seller enters to sell.
 @app.route('/submitSwipes', methods=['GET', 'POST'])
